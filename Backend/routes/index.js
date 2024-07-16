7811851ff19558e307653283f6be8c52195d9995
Manar <manarbouoni@gmail.com>
2024-07-16 19:20:36 +0100

    signup front & back done

---
+++ Backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+const userSignUpController = require("../controller/userSignUp")
+router.post("/signup",userSignUpController)
+
+
+
+
+
+
+
+
+
+module.exports = router
